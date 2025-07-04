2da9c405ff12aa9ae3e3adbd97748d582b24ae98
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-04 17:20:07 +0200

    [BZRIO-166] Migrate to ASP.NET Core (#1)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-166
    * Remove Spring Boot gateway
    * Add ASP.NET Core with YARP gateway

---
+++ src/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Bazario.ApiGateway.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddRoutesConfigurationFiles(
+            this IConfigurationBuilder builder)
+        {
+            builder.AddJsonFile("Configurations/clusters.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/identity.routes.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/users.routes.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/listings.routes.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/search.routes.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("Configurations/complaints.routes.json", optional: true, reloadOnChange: true);
+
+            return builder;
+        }
+    }
+}
